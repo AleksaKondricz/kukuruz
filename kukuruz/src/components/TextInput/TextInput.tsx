cdc400af0ff4855c56850d49fe156cb27584ea1c
AleksaKondricz <al.kondricz@hotmail.com>
2022-12-01 16:33:06 +0100

    Text Input story written

---
+++ kukuruz/src/components/TextInput/TextInput.tsx
@@ -0,0 +1,28 @@
+import React, { HTMLAttributes } from 'react'
+import { IconCircleCheck, IconAlertCircle, IconUser } from '@tabler/icons'
+
+import './styles/TextInput.scss'
+
+export interface TextInputProps extends HTMLAttributes<HTMLInputElement> {
+    type: 'text' | 'password' | 'number';
+    label?: string;
+    placeholder?: string;
+    size?: 'sm' | 'md' | 'lg';
+    state?: 'untouched' | 'focused' | 'disabled' | 'success' | 'error';
+    withIcon?: boolean;
+
+
+}
+
+export const TextInput = ({ type, label, placeholder, size, state, withIcon }: TextInputProps) => {
+    const classNames = `text-input-wrapper text-input-${size} text-input-${state} ${withIcon && 'text-input-with-icon'}`
+    return (
+        <div className={classNames}>
+            {withIcon && <div className='icon-start'><IconUser stroke='1.8' color='#33333350' fill='#33333350' size='18' /></div>}
+            <label>{label}</label>
+            <input type={type} placeholder={placeholder} />
+            {state === 'success' && <IconCircleCheck stroke='1.8' size='18' color='#008D4C' />}
+            {state === 'error' && <IconAlertCircle stroke='1.8' size='18' color='#cc444b' />}
+        </div>
+    )
+}
---
+++ kukuruz/src/components/TextInput/stories/TextInput.stories.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Meta, Story } from '@storybook/react'
+import { TextInput, TextInputProps } from '../TextInput';
+import '../styles/TextInput.scss';
+
+//meta data with the type Meta for our text input component;
+const meta: Meta = {
+    title: 'TextInput',
+    component: TextInput,
+    argTypes: {
+        children: {
+            defaultValue: 'Default button'
+        }
+    }
+}
+
+export default meta;
+
+const Template: Story<TextInputProps> = (args) => <TextInput {...args} />
+
+
+//Playground TextInput
+export const TextInputPlayground = Template.bind({})
+
+//Types of TextInput
+export const TextInputTypes = () => (
+    <div className='inputs-wrapper'>
+        <TextInput label='Text input' type='text' placeholder='I am a text input ...' size='sm' />
+        <TextInput label='Number input' type='number' placeholder='I am a number input ...' size='sm' />
+        <TextInput label='Password input' type='password' placeholder='I am a password input ...' size='sm' />
+    </div>
+)
+
+//Sizes of TextInput
+export const TextInputSizes = () => (
+    <div className='inputs-wrapper'>
+        <TextInput type='text' placeholder='I am a small text input ...' size='sm' />
+        <TextInput type='text' placeholder='I am a small text input ...' size='md' />
+        <TextInput type='text' placeholder='I am a small text input ...' size='lg' />
+    </div>
+)
+
+//States of TextInput
+export const TextInputStates = () => (
+    <div className='inputs-wrapper'>
+        <TextInput type='text' placeholder="I'm untouched input ..." size='sm' />
+        <TextInput type='text' placeholder="I'm focused input ..." size='sm' state='focused' />
+        <TextInput type='text' placeholder="I'm disabled input ..." size='sm' state='disabled' />
+        <TextInput type='text' placeholder="I'm success" size='sm' state='success' />
+        <TextInput type='text' placeholder="Oh no! There is an error" size='sm' state='error' />
+    </div>
+)
+
+//With or without icon
+export const TextInputWithIcon = () => (
+    <TextInput type='text' withIcon placeholder='Input with icon ... ' size='sm' />
+)
