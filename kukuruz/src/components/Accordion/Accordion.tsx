b7a8f0e9af76b4e981a1593dacc2bc38a7822959
AleksaKondricz <al.kondricz@hotmail.com>
2022-12-14 13:55:49 +0100

    accordion finished

--- kukuruz/src/components/Accordion/Accordion.tsx
+++ kukuruz/src/components/Accordion/Accordion.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {IconCirclePlus, IconCircleMinus} from '@tabler/icons'
+import { accordionDataMock } from './mocks/accordianDataMock';
 import './styles/Accordion.scss';
 
 export interface AccordionProps {
@@ -7,21 +8,30 @@ export interface AccordionProps {
 }
 
 export const Accordion = ({ }: AccordionProps) => {
-    const [isActive, setIsActive] = useState(false)
-    const classNames = ``
+    const [isActive, setIsActive] = useState(null)
+
+    const handleToggle = (id:any) => {
+        if(isActive === id){
+            setIsActive(null)
+            return false
+        }
+        setIsActive(id)
+    }
+
     return (
         <div className='accordion-wrapper'>
             <div className='accordion-box'>
                 <h2>An Accordion</h2>
-                <p>This is an accordion elemnet. One of the best ways of showing neadly data which may interest your end user. Perfect for using it as FAQ section.</p>
-                <div className='accordion-item'>
-                    <h4> <span onClick={()=>setIsActive(!isActive)}>{isActive ?<IconCircleMinus fill='#000' color='#fff'/> : <IconCirclePlus fill='#000' color='#fff'/>}</span> What is an accordion?</h4>
-                    {isActive && <p> In web design, an accordion is a type of menu that displays a list of headers stacked on top of one another. When clicked on (or triggered by a keyboard interaction or screen reader), these headers will either reveal or hide associated content.</p>}
-                </div>
-                <div className='accordion-item'>
-                    <h4> <span onClick={()=>setIsActive(!isActive)}>{isActive ?<IconCircleMinus fill='#000' color='#fff'/> : <IconCirclePlus fill='#000' color='#fff'/>}</span> What are the best use cases for using an accordion?</h4>
-                    {isActive && <p> In web design, an accordion is a type of menu that displays a list of headers stacked on top of one another. When clicked on (or triggered by a keyboard interaction or screen reader), these headers will either reveal or hide associated content.</p>}
-                </div>
+                <p>This is an accordion element. One of the best ways of showing neadly data which may interest your end user. Perfect for using it as FAQ section.</p>
+                {accordionDataMock.map(
+                    accord => (
+                        <div className='accordion-item' key={accord.id}>
+                        <h4> <span onClick={()=>handleToggle(accord.id)}>{isActive === accord.id ?<IconCircleMinus fill='#000' color='#fff'/> : <IconCirclePlus fill='#000' color='#fff'/>}</span> {accord.question}</h4>
+                        {isActive === accord.id && <p>{accord.answer}</p>}
+                    </div>
+                    )
+                )}
+
             </div>
         </div>
     )
---
+++ kukuruz/src/components/Accordion/mocks/accordianDataMock.ts
@@ -0,0 +1,13 @@
+export const accordionDataMock = [
+    {
+        id: '0',
+        question: 'What is an accordion?',
+        answer: 'In web design, an accordion is a type of menu that displays a list of headers stacked on top of one another. When clicked on (or triggered by a keyboard interaction or screen reader), these headers will either reveal or hide associated content.'
+
+    },
+    {
+        id: '1',
+        question: 'What are the best use cases for using an accordion?',
+        answer: 'Accordions are most appropriate when users will only need a few specific pieces of content within a page or if you have only a small space to display a lot of content. According to the Nielsen Group,when used properly, accordions offer benefits to the user experience, including: Collapsing the page minimizes scrolling.'
+    },
+]
